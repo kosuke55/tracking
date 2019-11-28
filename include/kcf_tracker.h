fffbbc02401f1c8dc69ce5e9b0c06d029d66c7fa
kosuke55 <kosuke.tnp@gmail.com>
2019-11-29 03:25:59 +0900

    pub roi

--- include/kcf_tracker.h
+++ include/kcf_tracker.h
@@ -2,6 +2,7 @@
 #define _KCF_ROS_KCF_TRACKER_
 
 #include <stdlib.h>
+#include <string>
 
 #include <ros/ros.h>
 #include <nodelet/nodelet.h>
@@ -12,12 +13,14 @@
 
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/CameraInfo.h>
 #include <std_msgs/Header.h>
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
 #include <opencv2/tracking/tracker.hpp>
 #include <opencv2/tracking/tldDataset.hpp>
 #include <jsk_recognition_msgs/RectArray.h>
+
 
 
 namespace tracking
@@ -35,37 +38,37 @@ namespace tracking
       > ApproximateSyncPolicy;
 
   protected:
+
+    virtual void onInit();
+    virtual void callback(const sensor_msgs::Image::ConstPtr& raw_image_msg,
+                          const jsk_recognition_msgs::RectArray::ConstPtr& detected_boxes);
+    virtual void load_image(cv::Mat& image, const sensor_msgs::Image::ConstPtr& image_msg);
+    virtual void infoCallback(
+      const sensor_msgs::CameraInfo::ConstPtr& info_msg);
+
     cv::Ptr<cv::Tracker> tracker_;
-
     cv::Mat image_;
-
+    cv::TrackerKCF::Params params;
     std_msgs::Header header_;
-
+    sensor_msgs::CameraInfo::ConstPtr camera_info_;
+    std::string params_path_ = "~/catkin_ws/src/tracking/config/params.yml";
+    std::string input_camera_info_ = "/head_mount_kinect/hd/camera_info";
     ros::NodeHandle nh_;
     ros::NodeHandle pnh_;
-
     ros::Publisher image_pub_;
+    ros::Publisher pub_camera_info_;
+    // ros::Subscriber<sensor_msgs::CameraInfo, KcfTracker> sub_info_;
+    ros::Subscriber sub_info_;
     cv::Rect2d roi_;
-    bool missing_ = true;
-
     boost::mutex mutex_;
-
-    bool is_approximate_sync_ = true;
-    std::string params_path_ = "~/catkin_ws/src/tracking/config/params.yml";
-    cv::TrackerKCF::Params params;
-
     boost::shared_ptr<
       message_filters::Synchronizer<SyncPolicy> > sync_;
     boost::shared_ptr<
       message_filters::Synchronizer<ApproximateSyncPolicy> > approximate_sync_;
     message_filters::Subscriber<sensor_msgs::Image> sub_image_;
     message_filters::Subscriber<jsk_recognition_msgs::RectArray> sub_boxes_;
-
-    virtual void onInit();
-    virtual void callback(const sensor_msgs::Image::ConstPtr& raw_image_msg,
-                          const jsk_recognition_msgs::RectArray::ConstPtr& detected_boxes);
-
-    virtual void load_image(cv::Mat& image, const sensor_msgs::Image::ConstPtr& image_msg);
+    bool missing_ = true;
+    bool is_approximate_sync_ = true;
 
   private:
   }; // class KcfTracker
--- src/kcf_tracker.cpp
+++ src/kcf_tracker.cpp
@@ -10,24 +10,26 @@ void KcfTracker::onInit()
   pnh_ = getPrivateNodeHandle();
   pnh_.getParam("approximate_sync", is_approximate_sync_);
   pnh_.getParam("params_path", params_path_);
+  pnh_.getParam("input_camera_info", input_camera_info_);
   ROS_INFO("params_path_:%s", params_path_.c_str());
   tracker_ = cv::TrackerKCF::create(params);
   cv::FileStorage fs(params_path_, cv::FileStorage::READ);
   cv::FileNode features = fs["my_object"];
   // tracker_->read(features);
   tracker_->save("/home/kosuke/catkin_ws/src/tracking/launch/fuga.yml");
-
   image_pub_ = pnh_.advertise<sensor_msgs::Image>("output_image", 1);
-  // sub_image_.subscribe(pnh_,
-  //                      "/head_mount_kinect/hd/image_color_rect_repub_desktop", 1);
+  pub_camera_info_ = pnh_.advertise<sensor_msgs::CameraInfo>(
+      "output/camera_info", 1);
   sub_image_.subscribe(pnh_,
-                       "/apply_mask_image_in_gripper/output", 1);
+                       "/apply_mask_image_in_gripper/output", 100);
   sub_boxes_.subscribe(pnh_,
-                       "/mask_rcnn_instance_segmentation/output/rects", 1);
+                       "/mask_rcnn_instance_segmentation/output/rects", 100);
+  sub_info_ = nh_.subscribe(input_camera_info_, 1,
+                            &KcfTracker::infoCallback, this);
   if (is_approximate_sync_)
   {
     approximate_sync_ =
-      boost::make_shared<message_filters::Synchronizer<ApproximateSyncPolicy> >(1);
+      boost::make_shared<message_filters::Synchronizer<ApproximateSyncPolicy> >(100);
     approximate_sync_->connectInput(sub_image_,
                                     sub_boxes_);
     approximate_sync_->registerCallback(boost::bind
@@ -59,13 +61,19 @@ void KcfTracker::load_image(cv::Mat& image, const sensor_msgs::Image::ConstPtr& 
   }
 }
 
+void KcfTracker::infoCallback(
+    const sensor_msgs::CameraInfo::ConstPtr& info_msg)
+{
+  boost::mutex::scoped_lock lock(mutex_);
+  camera_info_ = info_msg;
+}
+
 void KcfTracker::callback(const sensor_msgs::Image::ConstPtr& raw_image_msg,
                           const jsk_recognition_msgs::RectArray::ConstPtr& detected_rects)
 {
   header_ = raw_image_msg->header;
   image_.release();
   load_image(image_, raw_image_msg);
-  std::cout << params.detect_thresh << std::endl;
   auto rects = detected_rects->rects;
   if (rects.size() != 0)
   {
@@ -74,12 +82,13 @@ void KcfTracker::callback(const sensor_msgs::Image::ConstPtr& raw_image_msg,
                       rects[0].y - 10,
                       rects[0].width + 20,
                       rects[0].height + 20);
+
     tracker_ = cv::TrackerKCF::create(params);
     tracker_->init(image_, roi_);
     missing_ = ! tracker_->update(image_, roi_);
     cv::rectangle(image_, roi_,
                   CV_RGB(0,255,0), 2);
-    cv::putText(image_, "redetect", cv::Point(10,300),
+    cv::putText(image_, "redetect", cv::Point(10,200),
                 cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0,0,200), 2, CV_AA);
     // missing_ = false;
   }
@@ -89,19 +98,29 @@ void KcfTracker::callback(const sensor_msgs::Image::ConstPtr& raw_image_msg,
     missing_ = ! tracker_->update(image_, roi_);
     cv::rectangle(image_, roi_,
                   CV_RGB(0,255,0), 2);
-    cv::putText(image_, "tracking", cv::Point(10,300),
+    cv::putText(image_, "tracking", cv::Point(10,200),
                 cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0,0,200), 2, CV_AA);
   }
   else if (missing_)
   {
     ROS_INFO("misisng");
     // missing_ = ! tracker_->update(image_, roi_);
-    cv::putText(image_, "missing", cv::Point(10,300),
+    cv::putText(image_, "missing", cv::Point(10,200),
                 cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0,0,200), 2, CV_AA);
   }
   image_pub_.publish(cv_bridge::CvImage(header_,
                                         sensor_msgs::image_encodings::BGR8,
                                         image_).toImageMsg());
+  if (camera_info_)
+  {
+    sensor_msgs::CameraInfo ci_roi(*camera_info_);
+    ci_roi.roi.x_offset = roi_.x;
+    ci_roi.roi.y_offset = roi_.y;
+    ci_roi.roi.width = roi_.width;
+    ci_roi.roi.height = roi_.height;
+    ci_roi.roi.do_rectify = true;
+    pub_camera_info_.publish(ci_roi);
+  }
 }
 }
 
